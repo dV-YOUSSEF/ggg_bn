7c0df62587c501b3ae2f36ab34192f8a1a626e01
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-19 03:14:26 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["JOO_B_R_Z", "JOO_B_R_Z"]
+OWNER_NAME = "𝗬.𝗢.𝗨.𝗦.𝗦.𝗘.𝐅"
+BOT_TOKEN = "6853708481:AAERyLYQimGnfEX5cF8eV20g0n1KRLkFb44"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/BPHEE"
+GROUP = "https://t.me/JJJKD1"
+VIDEO = "https://t.me/JJJKD1/2797"
+LOGS = "JJJKD1"
+
