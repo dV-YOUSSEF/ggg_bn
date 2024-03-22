dccb19be582877db1276c761b4232a4f78b451d7
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-22 21:34:17 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,7 +2,7 @@
 OWNER = ["JOO_B_R_Z", "JOO_B_R_Z"]
 OWNER_NAME = "𝗬.𝗢.𝗨.𝗦.𝗦.𝗘.𝐅"
 BOT_TOKEN = "6853708481:AAERyLYQimGnfEX5cF8eV20g0n1KRLkFb44"
-DATABASE = "mongodb+srv://youssefsv3:<password>@cluster0.ywaql6m.mongodb.net/?retryWrites=true&w=majority"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/BPHEE"
 GROUP = "https://t.me/B_X_N1"
 VIDEO = "B_X_N1"
