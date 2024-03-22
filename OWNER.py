7ff9248fdf581e4c16463d24ee77617338701089
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-22 16:30:09 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,9 +2,9 @@
 OWNER = ["JOO_B_R_Z", "JOO_B_R_Z"]
 OWNER_NAME = "𝗬.𝗢.𝗨.𝗦.𝗦.𝗘.𝐅"
 BOT_TOKEN = "6853708481:AAERyLYQimGnfEX5cF8eV20g0n1KRLkFb44"
-DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+DATABASE = "mongodb+srv://bot_vambir:Al2552001@cluster0.heabj.mongodb.net/vambir_bot?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/BPHEE"
-GROUP = "https://t.me/JJJKD1"
-VIDEO = "https://t.me/JJJKD1/2797"
-LOGS = "JJJKD1"
+GROUP = "https://t.me/B_X_N1"
+VIDEO = "B_X_N1"
+LOGS = "B_X_N1"
 
